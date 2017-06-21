e488ee66fe7529082221291985a9366b61c61ed3
Unknown <zhengchen.davi@gmail.com>
2017-06-21 22:10:01 +0800

    RCM点位置算法更新为projection point法
    采用singular robust，Vrcmp指向理论RCM点
    考虑s_tube_fir=0情况，此时tube_theta_fir应同时为0
    加入对error_r 和error_p变量的记录分析
    加入对Vrcmp和(p_rcm_origin-p_rcm)是否共线的夹角分析

--- denso_cal_moved_rcmpoint_for_dexerity_verify.m
+++ denso_cal_moved_rcmpoint_for_dexerity_verify.m
@@ -38,7 +38,6 @@ p_0_o6=p_0_o5+d6*R_0_6*z_axis;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%
 % RCM point
-npts=1000;
 s_rcm=tube_para(1); % RCM点弧长
 s_tube_fir=tube_para(2);
 s_tube_sec=tube_para(3);
@@ -46,45 +45,88 @@ tube_theta_fir=tube_para(4);
 tube_theta_sec=tube_para(5);
 tube_r_fir=s_tube_fir/tube_theta_fir; % 第一段圆弧半径
 tube_r_sec=s_tube_sec/tube_theta_sec; % 第二段圆弧半径
-lintube_theta_fir=linspace(0,tube_theta_fir,npts/2);
-lintube_theta_sec=linspace(0,tube_theta_sec,npts/2);
 if tube_theta_fir==0
-    p_6_t2_fir=180*z_axis*ones(1,npts/2)+[zeros(1,npts/2);zeros(1,npts/2);linspace(0,s_tube_fir,npts/2)];
-    p_6_t2_sec=p_6_t2_fir(:,npts/2)*ones(1,npts/2)+rotx(-tube_theta_fir)*[zeros(1,npts/2);tube_r_sec-cos(lintube_theta_sec)*tube_r_sec;sin(lintube_theta_sec)*tube_r_sec];
-elseif tube_theta_sec==0
-    p_6_t2_fir=180*z_axis*ones(1,npts/2)+[zeros(1,npts/2);tube_r_fir-cos(lintube_theta_fir)*tube_r_fir;sin(lintube_theta_fir)*tube_r_fir];
-    p_6_t2_sec=p_6_t2_fir(:,npts/2)*ones(1,npts/2)+rotx(-tube_theta_fir)*[zeros(1,npts/2);zeros(1,npts/2);linspace(0,s_tube_sec,npts/2)];
+    p_6_startpoint_fir=[0;0;0];
+    p_6_endpoint_fir=[0;0;s_tube_fir];
 else
-    p_6_t2_fir=180*z_axis*ones(1,npts/2)+[zeros(1,npts/2);tube_r_fir-cos(lintube_theta_fir)*tube_r_fir;sin(lintube_theta_fir)*tube_r_fir];
-    p_6_t2_sec=p_6_t2_fir(:,npts/2)*ones(1,npts/2)+rotx(-tube_theta_fir)*[zeros(1,npts/2);tube_r_sec-cos(lintube_theta_sec)*tube_r_sec;sin(lintube_theta_sec)*tube_r_sec];
+    p_6_startpoint_fir=[0;0;0];
+    p_6_endpoint_fir=[0;tube_r_fir-cos(tube_theta_fir)*tube_r_fir;sin(tube_theta_fir)*tube_r_fir];
 end
-p_6_t2=[p_6_t2_fir,p_6_t2_sec];
-p_6_t_rcm=p_6_t2-(R_0_6'*(p_rcm_origin-p_0_o6))*ones(1,npts);
-for i=1:npts
-    d_6_t_rcm(i)=norm(p_6_t_rcm(:,i));
-end
-[min_v,min_i]=min(d_6_t_rcm);
-p_6_rcmafter=p_6_t2(:,min_i)-180*z_axis;
-if min_i<=npts/2
-    if tube_theta_fir==0
-        s_rcm_after_fir=p_6_rcmafter(3);
-    else
-        s_rcm_after_fir=asin(p_6_rcmafter(3)/tube_r_fir)*tube_r_fir;
-    end
-    s_rcm_after=s_rcm_after_fir;
+if tube_theta_sec==0
+    p_fir_startpoint_sec=[0;0;0];
+    p_fir_endpoint_sec=[0;0;s_tube_sec];
 else
-    if tube_theta_fir==0
-        p_cir1_cir1rcm=(rotx(-tube_theta_fir))'*(p_6_rcmafter-[0;0;s_tube_fir]);
-    else
-        p_cir1_cir1rcm=(rotx(-tube_theta_fir))'*(p_6_rcmafter-[0;tube_r_fir-cos(tube_theta_fir)*tube_r_fir;sin(tube_theta_fir)*tube_r_fir]);
-    end
-    if tube_theta_sec==0
-        s_rcm_after_sec=p_cir1_cir1rcm(3);
-    else
-        s_rcm_after_sec=asin(p_cir1_cir1rcm(3)/tube_r_sec)*tube_r_sec;
-    end
-    s_rcm_after=s_tube_fir+s_rcm_after_sec;
+    p_fir_startpoint_sec=[0;0;0];
+    p_fir_endpoint_sec=[0;tube_r_sec-cos(tube_theta_sec)*tube_r_sec;sin(tube_theta_sec)*tube_r_sec];
 end
+
+p_6_rcmorin=R_0_6'*(p_rcm_origin-p_0_o6)-180*z_axis; % p_0_o6+R_0_6*(180*z_axis+p_6_rcmorin)=p_rcm_oringin
+
+if tube_theta_fir==0
+    p_6_projectpoint_fir=[0;0;p_6_rcmorin(3)];
+    s_rcm_fir=p_6_rcmorin(3);
+    if s_rcm_fir>=0 && s_rcm_fir<=s_tube_fir
+        d_pp_rcm_fir=norm(p_6_rcmorin-p_6_projectpoint_fir);
+    else
+        [d_pp_rcm_fir,i_fir]=min([norm(p_6_rcmorin-p_6_startpoint_fir),norm(p_6_rcmorin-p_6_endpoint_fir)]);
+        if i_fir==1
+            s_rcm_fir=0;
+        else
+            s_rcm_fir=s_tube_fir;
+        end
+    end
+else %目前并没考虑圆弧超过90度后projection point位置的计算方法；并没考虑RCM点在垂直于圆弧所在平面且经过圆弧圆心的直线上的情况，因为RCM点不可能离圆弧那么远
+    p_6_projectpoint_fir=[0;sign(p_6_rcmorin(3))*abs(tube_r_fir)/sqrt((p_6_rcmorin(2)-tube_r_fir)^2+p_6_rcmorin(3)^2)*(p_6_rcmorin(2)-tube_r_fir)+tube_r_fir;sign(p_6_rcmorin(3))*abs(tube_r_fir)/sqrt((p_6_rcmorin(2)-tube_r_fir)^2+p_6_rcmorin(3)^2)*p_6_rcmorin(3)];
+    s_rcm_fir=asin(p_6_projectpoint_fir(3)/tube_r_fir)*tube_r_fir;
+    if s_rcm_fir>=0 && s_rcm_fir<=s_tube_fir
+        d_pp_rcm_fir=norm(p_6_rcmorin-p_6_projectpoint_fir);
+    else
+        [d_pp_rcm_fir,i_fir]=min([norm(p_6_rcmorin-p_6_startpoint_fir),norm(p_6_rcmorin-p_6_endpoint_fir)]);
+        if i_fir==1
+            s_rcm_fir=0;
+        else
+            s_rcm_fir=s_tube_fir;
+        end
+    end
+end
+
+p_fir_rcmorin=(rotx(-tube_theta_fir))'*(p_6_rcmorin-p_6_endpoint_fir); % rotx(-tube_theta_fir)*p_fir_rcmorin+p_6_endpoint_fir=p_6_rcmorin
+
+if tube_theta_sec==0
+    p_fir_projectpoint_sec=[0;0;p_fir_rcmorin(3)];
+    s_rcm_sec=p_fir_rcmorin(3);
+    if s_rcm_sec>=0 && s_rcm_sec<=s_tube_sec
+        d_pp_rcm_sec=norm(p_fir_rcmorin-p_fir_projectpoint_sec);
+    else
+        [d_pp_rcm_sec,i_sec]=min([norm(p_fir_rcmorin-p_fir_startpoint_sec),norm(p_fir_rcmorin-p_fir_endpoint_sec)]);
+        if i_sec==2
+            s_rcm_sec=s_tube_sec;
+        else
+            s_rcm_sec=0;
+        end
+    end
+else
+    p_fir_projectpoint_sec=[0;sign(p_fir_rcmorin(3))*abs(tube_r_sec)/sqrt((p_fir_rcmorin(2)-tube_r_sec)^2+p_fir_rcmorin(3)^2) *(p_fir_rcmorin(2)-tube_r_sec)+tube_r_sec;sign(p_fir_rcmorin(3))*abs(tube_r_sec)/sqrt((p_fir_rcmorin(2)-tube_r_sec)^2+p_fir_rcmorin(3)^2)*p_fir_rcmorin(3)];
+    s_rcm_sec=asin(p_fir_projectpoint_sec(3)/tube_r_sec)*tube_r_sec;
+    if s_rcm_sec>=0 && s_rcm_sec<=s_tube_sec
+        d_pp_rcm_sec=norm(p_fir_rcmorin-p_fir_projectpoint_sec);
+    else
+        [d_pp_rcm_sec,i_sec]=min([norm(p_fir_rcmorin-p_fir_startpoint_sec),norm(p_fir_rcmorin-p_fir_endpoint_sec)]);
+        if i_sec==2
+            s_rcm_sec=s_tube_sec;
+        else
+            s_rcm_sec=0;
+        end
+    end
+end
+
+if d_pp_rcm_fir<=d_pp_rcm_sec
+    s_rcm_after=s_rcm_fir;
+else
+    s_rcm_after=s_tube_fir+s_rcm_sec;
+end
+    
+
 tube_para=[s_rcm_after,s_tube_fir,s_tube_sec,tube_theta_fir,tube_theta_sec];
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
